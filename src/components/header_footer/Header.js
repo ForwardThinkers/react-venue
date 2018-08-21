802009a394cff1aa7b4b15593b2d3d7ce8696565
Warren Leyes <2774431+WLeyes@users.noreply.github.com>
2018-08-20 21:57:33 -0700

    added header

---
+++ src/components/header_footer/Header.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import MenuIcon from '@material-ui/icons/Menu';
+import IconButton from '@material-ui/core/IconButton';
+
+import SideDrawer from './SideDrawer';
+
+class Header extends Component {
+
+  state = {
+    drawerOpen: false,
+    headerTransparent: false
+  }
+
+  componentDidMount(){
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = () => {
+    if(window.scrollY) {
+      this.setState({ headerTransparent: true })
+    } else {
+      this.setState({ headerTransparent: false })
+    }
+  }
+
+  toggleDrawer = (value) => {
+    this.setState({
+      drawerOpen: value
+    });
+  }
+
+  render() {
+    return (
+      <AppBar
+      position="fixed"
+      style={{
+        backgroundColor: this.state.headerTransparent ? '#2f2f2f' : 'transparent' ,
+        boxShadow: 'none',
+        padding: '10px 0'
+      }}
+      >
+      <Toolbar>
+        <div className="header_logo">
+          <div className="font_righteous header_logo_venue">The Venue</div>
+          <div className="header_logo_title">Musical Events</div>
+        </div>
+        <IconButton
+        aria-label="Menu"
+        color="inherit"
+        onClick={ () => this.toggleDrawer(true) }
+        >
+          <MenuIcon/>
+        </IconButton>
+        <SideDrawer
+        open={ this.state.drawerOpen }
+        onClose={ value => this.toggleDrawer(value) }
+        />
+      </Toolbar>
+      </AppBar>
+    );
+  }
+}
+
+export default Header;
